ca51a94bc26a1127f218fa6205585b04d3a4126e
EmilDanielSamfirescu <emil.danielsam@gmail.com>
2023-07-13 22:01:04 +0200

    header fatto!

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -14,10 +14,19 @@ methods: {
 
 <template>
     <footer>
-        FOOTER
+        <section class="top-footer">top-footer</section>
+        <section class="center-footer">center-footer</section>
+        <section class="bottom-footer">bottom-footer</section>
     </footer>
 </template>
 
 <style lang="scss" scoped>
+@use "../assets/scss/partials/variables.scss" as *;
+.top-footer {
+    background-color: $footer-bg-blue;
+}
+.bottom-footer {
+    background-color: $footer-bg-black;
+}
 
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,6 +3,18 @@ export default {
     name: "HeaderComponent",
 data (){
     return {
+        headerNavLink: [
+            `characters`,
+            `comics`,
+            `movies`,
+            `tv`,
+            `games`,
+            `collectables`,
+            `videos`,
+            `fans`,
+            `news`,
+            `shop`
+        ],
 
     }
 },
@@ -14,10 +26,76 @@ methods: {
 
 <template>
     <header>
-        HEADER
+        <div class="container">
+            <div class="row">
+
+                <div class="logo-container">
+                    <img src="../assets/img/dc-logo.png" alt="">
+                </div>
+                <nav class="link-container">
+                    <ul class="">
+                        <li v-for="(singleLink, i) in headerNavLink">
+                            <a href="#">{{ singleLink }}</a>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+        </div>
     </header>
 </template>
 
 <style lang="scss" scoped>
+@use "../assets/scss/partials/variables.scss" as *;
+@use "../assets/scss/partials/mixin.scss" as *;
+
+
+
+.container {
+    margin: auto;
+}
+
+.row {
+    display: flex;
+    justify-content: space-between;;
+    align-items: center;
+}
+
+.logo-container {
+    width: 100px;
+    height: auto;
+    padding: 10px;
+
+    img {
+        width: 100%;
+        display: block;
+    }
+}
+
+
+
+ul {
+    font-size: 0.8em;
+    padding: 10px;
+    height: 100%;
+
+    li {
+        list-style: none;
+        display: inline-block;
+
+        a {
+            color: $header-color-link;
+            text-decoration: none;
+            text-transform: uppercase;
+            font-weight: bold;
+            padding: 50px 10px;
+        }
+
+        a:hover {
+            color: $footer-bg-blue;
+            border-bottom: 3px solid $footer-bg-blue;
+        }
+    }
+}
 
 </style>
